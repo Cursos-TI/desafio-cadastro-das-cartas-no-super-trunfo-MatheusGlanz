22dedfb26197ab5cd632beebd4525f5167add6cb
Matheus Luiz Fernandes Vieira <202401401171@alunos.estacio.br>
2025-03-13 19:14:25 +0000

    Meu Codigo Nivel Aventureiro

--- Aventureiro/aventureiro.c
+++ Aventureiro/aventureiro.c
@@ -20,9 +20,9 @@ int main(){
     printf("Digite o estado da carta 1 (A-H):");
     scanf("%c", &estadoCarta1);
     printf("Digite o codigo da carta 1 (ex: A01):");
-    scanf("%s", &codigo);
+    scanf("%s", codigo);
     printf("Digite o nome da cidade da carta 1:");
-    scanf("%s", &cidade);
+    scanf("%s", cidade);
     printf("Digite a população da cidade da carta 1:");
     scanf("%f", &populacaoCarta1);
     printf("Digite a área da cidade da carta 1 (em km²):");
@@ -36,9 +36,9 @@ int main(){
     printf("Digite o estado da carta 2 (A-H):");
     scanf("%c", &estadoCarta2);
     printf("Digite o codigo da carta 2 (ex: A01):");
-    scanf("%s", &codigo);
+    scanf("%s", codigo);
     printf("Digite o nome da cidade da carta 2:");
-    scanf("%s", &cidade);
+    scanf("%s", cidade);
     printf("Digite a população da cidade da carta 2:");
     scanf("%f", &populacaoCarta2);
     printf("Digite a área da cidade da carta 2 (em km²):");
@@ -61,7 +61,7 @@ int main(){
        printf("Área: %.2f km²\n", areaCarta1);
        printf("PIB: R$:%.2f\n", PIBCarta1);
        printf ("Pontos Turísticos: %d\n", pontosturisticosCarta1);
-       printf("Dencidade Populacional: %.2f Pessoas\n", dencidadepopulacionalCarta1);
+       printf("Dencidade Populacional: %.2f\n", dencidadepopulacionalCarta1);
        printf("PIB per Capta: R$:%.2f\n", PIBpercaptaCarta1);
 
      //Exibição carta 2
@@ -73,11 +73,11 @@ int main(){
      printf("Estado: %c\n", estadoCarta2);
      printf("Código: %s\n", codigo);
      printf("Cidade: %s\n", cidade);
-     printf("População: (%.2f Pessoas)\n", populacaoCarta2);
+     printf("População: %.2f Pessoas\n", populacaoCarta2);
      printf("Área: %.2fkm²\n", areaCarta2);
      printf("PIB: R$:%.2f\n", PIBCarta2);
      printf ("Pontos Turísticos: %d\n", pontosturisticosCarta2);
-     printf("Dencidade Populacional: %.2f Pessoas\n", dencidadepopulacionalCarta2);
+     printf("Dencidade Populacional: %.2f\n", dencidadepopulacionalCarta2);
      printf("PIB per Capta: R$:%.2f\n", PIBpercaptaCarta2);
 
 
